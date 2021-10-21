90d2f6cd0bbc52cc89cf9f483589fc977c63d11d
maoleszc <marcin.oleszczuk@avid.com>
2021-10-21 21:59:51 +0200

    added loging to logstash through elastic-search and kibana.

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,12 +1,30 @@
 import * as winston from "winston"
 import * as expressWinston from "express-winston"
+import * as logform from "logform"
+import { LogstashTransport } from "winston-logstash-ts"
 
-export let logger;
+export let logger:ReturnType<typeof winston.createLogger>;
 export const initWinstonLogger = async () => {
     logger = winston.createLogger({
         transports: [
-          new winston.transports.Console()
+          new winston.transports.Console({handleExceptions: true}),
+          new LogstashTransport({
+            host: "localhost",
+            port: 5000,
+            protocol: "tcp",
+            format: logform.format.combine(
+                logform.format.timestamp(),
+                logform.format.logstash(),
+            )
+        })
         ],
+        levels: {error : 0},
+        silent: false,
+        handleExceptions: true,
+        level: "error",
+        exceptionHandlers: (obj) => {
+          console.log(obj)
+        },
         format: winston.format.combine(
           winston.format.colorize(),
           winston.format.json()
@@ -18,7 +36,7 @@ export const initWinstonLogger = async () => {
 export const winstonLoggerMiddleware = 
     expressWinston.logger({
         transports: [
-          new winston.transports.Console()
+          new winston.transports.Console(),
         ],
         meta: true,
         msg: 'HTTP {{req.method}} {{req.url}}',
